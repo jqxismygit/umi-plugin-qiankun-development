b69400275643eada2cbcebfcef2fac38fefb00c3
king <117745900@qq.com>
2020-12-24 15:08:56 +0800

    feat: 添加多个包的externals

--- example/pages/index.tsx
+++ example/pages/index.tsx
@@ -1,11 +1,79 @@
 import React from 'react';
-import styles from './index.css';
-import { Button } from 'antd';
+import {
+  G2,
+  Chart,
+  Geom,
+  Axis,
+  Tooltip,
+  Coord,
+  Label,
+  Legend,
+  View,
+  Guide,
+  Shape,
+  Facet,
+  Util,
+} from 'bizcharts';
+import DataSet from '@antv/data-set';
 
-export default () => (
+import { get } from 'lodash';
 
-  <div className={styles.normal}>
-    Hello Umi!111
-    <Button>btn</Button>
-  </div>
-);
+class Basic extends React.Component {
+  render() {
+    const data = [
+      {
+        country: '中国',
+        population: 131744,
+      },
+      {
+        country: '印度',
+        population: 104970,
+      },
+      {
+        country: '美国',
+        population: 29034,
+      },
+      {
+        country: '印尼',
+        population: 23489,
+      },
+      {
+        country: '巴西',
+        population: 18203,
+      },
+    ];
+    const obj = { a: 1 };
+    const a = get(obj, 'a');
+    console.log('a = ', a);
+    const ds = new DataSet();
+    const dv = ds.createView().source(data);
+    dv.source(data).transform({
+      type: 'sort',
+
+      callback(a, b) {
+        // 排序依据，和原生js的排序callback一致
+        return a.population - b.population > 0;
+      },
+    });
+    return (
+      <div style={{ width: 200, height: 240 }}>
+        <Chart
+          data={dv}
+          height={240}
+          padding={['auto', 45, 'auto', 'auto']}
+          forceFit
+        >
+          <Coord transpose />
+          <Axis name="country" />
+          <Axis name="population" visible={false} />
+          <Tooltip />
+          <Geom type="interval" position="country*population">
+            <Label content="population" offset={5} />
+          </Geom>
+        </Chart>
+      </div>
+    );
+  }
+}
+
+export default Basic;
